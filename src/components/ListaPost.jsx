6af119d7b900df336ec31e68a7600a047e17f22a
Natália Sens Weise <nataliaweise@gmail.com>
2022-08-09 18:42:01 -0300

    Changing tag a to Link and making requests to server

---
+++ src/components/ListaPost.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import {busca} from '../api/api'
+import '../assets/css/componentes/cartao.css'
+
+const ListaPost = ({url}) =>{
+    const [posts, setPosts] = useState([])
+
+    useEffect(()=>{
+        busca(url, setPosts)
+    }, [])
+
+    return(
+        <section className="posts container">
+            {posts.map((post)=>(
+                <Link className={`cartao-post cartao-post--${post.categoria}`}>
+                    <article className="cartao post" key={post.id}>
+                        <h3 className="cartao-post__titulo">{post.title}</h3>
+                        <p className="cartao-post__meta">{post.metadescription}</p>
+                    </article>
+                </Link>
+                ))}
+        </section>
+    )
+}
+
+export default ListaPost
